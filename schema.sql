118ca3eb497e4076b635b99f3e9a3ee2afb8f0bd
Oscar Delgado <oscy79@gmail.com>
2022-07-28 18:02:03 -0400

    7.3.1 update

--- schema.sql
+++ schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -32,3 +32,52 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 SELECT * FROM departments
+DROP TABLE dept_emp
+CREATE TABLE dept_emp (
+	 emp_no INT NOT NULL,
+     dept_no VARCHAR(4) NOT NULL,
+	 from_date DATE NOT NULL,
+     to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+CREATE TABLE titles (
+	 emp_no INT NOT NULL,
+     title VARCHAR NOT NULL,
+	 from_date DATE NOT NULL,
+     to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
+SELECT * FROM salaries
+select * FROM dept_emp
+select * From employees
+Select * from titles
+SELECT * FROM dept_manager
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
